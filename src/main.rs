37f350b9a07b4de5d2996d23fb6ff16b577c4fc1
likhon15 <likhon15-3916@diu.edu.bd>
2024-05-27 03:22:10 +0600

    added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+    let age = 31;
+    let name = "Jack";
+
+    // print the variables using println!
+    println!("Name = {}, Age = {}", name, age);
+    prac();
+    numbers();
+}
+
+fn prac() {
+    let age = 31;
+    let name = "Jack";
+
+    // print the variable using println!
+    println!("Name = {0}, Age = {1}", name, age);
+}
+
+
+
+fn numbers() {
+    // variable to store integer value
+    let age = 31;
+    println!("Age: {}", age);
+
+    // variable to store floating-point value
+    let salary = 342523.23;
+    println!("Salary: {}", salary);
+
+    // variable to store string
+    let name = "Jackie";
+    println!("Name: {}", name);
 }
